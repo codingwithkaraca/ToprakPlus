f4d386f1bc1d3d0ded8cac58f99ee6e48b6bfa87
codingwithkaraca <karacaalper806@gmail.com>
2025-04-11 23:24:23 +0300

    inital commit

---
+++ ToprakPlus.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ToprakPlus.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
